ee0b7844ddc2f7e45a0c73b33b9186cffb9396ee
tvliz123-lgtm <tvliz123@gmail.com>
2026-08-31 22:41:02 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@workspace/api-client-react": path.resolve(__dirname, "./src/api-client-react.ts"),
     },
   },
   build: {
